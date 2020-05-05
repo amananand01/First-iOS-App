13458ba49c171de98c8f43a3e3b568c67c6eb391
aman <aanand@ualberta.ca>
2020-05-05 15:27:47 -0600

    started warCard Game

---
+++ WarCardGame/WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by aman anand on 2020-05-03.
+//  Copyright © 2020 aman anand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
